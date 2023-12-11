c1dbdcf1e3637d3cd7df160e048f355bd354733d
mainul96 <110577655+mainul96@users.noreply.github.com>
2023-12-12 00:45:14 +0530

    insert data successfully

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         const serviceCollection = client.db('geniusCar').collection('services');
+        const orderCollection = client.db('geniusCar').collection('orders')
         app.get('/services', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
@@ -40,6 +41,13 @@ async function run() {
             const query = {_id: new ObjectId(id)};
             const service = await serviceCollection.findOne(query);
             res.send(service)
+        })
+
+        // Order
+        app.post('/orders', async(req, res)=>{
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result)
         })
     } finally {
 
